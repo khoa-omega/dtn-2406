23d0c3377ad9b63ca1c744809f09971300500921
khoa-omega <nvkhoa05@gmail.com>
2024-12-26 20:55:26 +0700

    [Database] Update lesson-03

--- Database/assignment-03.sql
+++ Database/assignment-03.sql
@@ -294,7 +294,12 @@ SELECT group_name
 FROM `group`
 WHERE created_date < "2019-12-20";
 
--- Question 7: Lấy ra ID của question có >= 4 câu trả lời
+-- Question 7: Lấy ra id của question có >= 4 câu trả lời
+SELECT question_id
+FROM answer
+GROUP BY question_id
+HAVING COUNT(answer_id) >= 4;
+
 -- Question 8: Lấy ra các mã đề thi có thời gian thi >= 60 phút
 -- và được tạo trước ngày 20/12/2019
 SELECT code
@@ -308,16 +313,36 @@ ORDER BY created_date DESC
 LIMIT 5;
 
 -- Question 10: Đếm số nhân viên thuộc department có id = 2
+SELECT COUNT(account_id) AS account_count
+FROM account
+WHERE department_id = 2;
+
 -- Question 11: Lấy ra nhân viên có tên bắt đầu bằng chữ "D"
 -- và kết thúc bằng chữ "o"
 SELECT *
 FROM account
 WHERE SUBSTRING_INDEX(full_name, " ", -1) LIKE "D%o";
 
--- Question 12: Xóa tất cả các exam được tạo trước ngày 20/12/2019
--- Question 13: Xóa tất cả các question có nội dung bắt đầu bằng từ "câu hỏi"
--- Question 14: Update thông tin của account có id = 5 thành tên "Nguyễn Bá Lộc" và
+-- Question 12: Xóa tất cả các exam
+-- được tạo trước ngày 20/12/2019
+DELETE FROM exam
+WHERE created_date < "2019-12-20";
+
+-- Question 13: Xóa tất cả các question
+-- có nội dung bắt đầu bằng từ "câu hỏi"
+DELETE FROM question
+WHERE content LIKE "câu hỏi%";
+
+-- Question 14: Update thông tin của account
+-- có id = 5 thành tên "Nguyễn Bá Lộc" và
 -- email thành loc.nguyenba@vti.com.vn
--- Question 15: update account có id = 5 sẽ thuộc group có id = 4
+UPDATE account
+SET full_name = "Nguyễn Bá Lộc",
+    email = "loc.nguyenba@vti.com.vn"
+WHERE account_id = 5;
 
-
+-- Question 15: Update account có id = 5
+-- sẽ thuộc group có id = 4
+UPDATE group_account
+SET group_id = 4
+WHERE account_id = 5;
--- Database/lesson-03.sql
+++ Database/lesson-03.sql
@@ -68,3 +68,45 @@ SELECT *
 FROM department
 ORDER BY department_id DESC
 LIMIT 1;
+
+-- Các hàm tổng hợp (AGGREGATE FUNCTION)
+-- COUNT, SUM, MIN, MAX, AVG
+-- COUNT(*): Đếm số dòng
+-- COUNT(duration): Đếm số dòng có duration khác NULL
+-- COUNT(DISTINCT duration): Đếm số dòng có duration khác nhau
+-- Chú ý: Áp dụng cho các giá trị khác NULL
+-- VD: Lấy ra số lượng thời gian thi
+SELECT 
+    COUNT(*),
+    COUNT(duration),
+    COUNT(DISTINCT duration),
+    COUNT(1),
+    SUM(duration),
+    MIN(duration),
+    MAX(duration),
+    AVG(duration)
+FROM exam;
+
+-- Mệnh đề GROUP BY
+-- VD: Thống kê mỗi chức vụ có bao nhiêu nhân viên
+SELECT position_id, COUNT(account_id) AS account_count
+FROM account
+GROUP BY position_id;
+
+-- Mệnh đề HAVING
+-- VD: Lấy ra chức vụ nhiều hơn 2 nhân viên
+SELECT position_id, COUNT(account_id) AS account_count
+FROM account
+GROUP BY position_id
+HAVING COUNT(account_id) > 2;
+
+-- Cập nhật dữ liệu
+-- VD: Đổi tên phòng ban số 1 thành "Phòng chờ"
+UPDATE department
+SET department_name = "Phòng chờ"
+WHERE department_id = 1;
+
+-- Xóa dữ liệu
+-- VD: Xóa phòng ban số 1
+DELETE FROM department
+WHERE department_id = 1;
